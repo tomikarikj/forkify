9f256e020f05ee8d40fdbe3b694e038f16010b0a
Tomi Karikj <karikjtomi@gmail.com>
2019-09-24 01:17:52 +0200

    Added logic for the search functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,40 @@
-const x = 23;
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, spinner, clearSpinner } from './views/base';
 
-console.log(7 ** 2);
+/** Global state
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-console.log(`this is a new number: ${x}`);
+// Form submit
+const onSearchSubmit = async () => {
+  // Get the query from the view
+  const query = searchView.getInput();
+  console.log(query);
+
+  if (query) {
+    // New search object and add it to the state
+    state.search = new Search(query);
+
+    // Clearing the UI and showing the spinner
+    searchView.clearInput();
+    searchView.clearResults();
+    spinner(elements.searchResults);
+
+    // Search for the recipes
+    await state.search.getResults();
+
+    // Render the results
+    clearSpinner();
+    searchView.renderResults(state.search.result);
+  }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+  e.preventDefault();
+  onSearchSubmit();
+});
