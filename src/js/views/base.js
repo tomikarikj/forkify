4e00508a7156b4691cd44eb191d16d7a3b69180e
Tomi Karikj <karikjtomi@gmail.com>
2019-09-24 12:23:22 +0200

    Added some more query selectors

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -2,7 +2,8 @@ export const elements = {
   searchForm: document.querySelector('.search'),
   searchInput: document.querySelector('.search__field'),
   searchResults: document.querySelector('.results'),
-  resultsList: document.querySelector('.results__list')
+  resultsList: document.querySelector('.results__list'),
+  resultsPages: document.querySelector('.results__pages')
 };
 
 export const classNames = {
