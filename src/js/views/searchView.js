4ffaeda1dfe885cf2655bc047dd6c7c3bd83aab2
Tomi Karikj <karikjtomi@gmail.com>
2019-09-24 12:23:43 +0200

    Finished the functionality for the pagination btns

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -6,6 +6,7 @@ export const clearInput = () => {
 };
 export const clearResults = () => {
   elements.resultsList.innerHTML = '';
+  elements.resultsPages.innerHTML = '';
 };
 
 const shortenTitle = (title, limit = 17) => {
@@ -43,6 +44,44 @@ const renderRecipe = recipe => {
   elements.resultsList.insertAdjacentHTML('beforeend', markup);
 };
 
-export const renderResults = recipes => {
-  recipes.forEach(renderRecipe);
+const createBtn = (page, btnType) => `
+  <button class="btn-inline results__btn--${btnType}" data-goto=${
+  btnType === 'prev' ? page - 1 : page + 1
+}>
+    <span>Page ${btnType === 'prev' ? page - 1 : page + 1}</span>
+    <svg class="search__icon">
+      <use href="img/icons.svg#icon-triangle-${
+        btnType === 'prev' ? 'left' : 'right'
+      }"></use>
+    </svg>
+  </button>
+`;
+
+const renderBtns = (page, numOfRecipes, recipesPerPage) => {
+  const pages = Math.ceil(numOfRecipes / recipesPerPage);
+  let btn;
+
+  if (page === 1 && pages > 1) {
+    // Button to go the next page
+    btn = createBtn(page, 'next');
+  } else if (page < pages) {
+    // Both buttons
+    btn = `
+      ${createBtn(page, 'prev')}
+      ${createBtn(page, 'next')}
+    `;
+  } else if (page === pages && pages > 1) {
+    // Button to go to the previous page
+    btn = createBtn(page, 'prev');
+  }
+
+  elements.resultsPages.insertAdjacentHTML('afterbegin', btn);
+};
+
+export const renderResults = (recipes, page = 1, recipesPerPage = 10) => {
+  const start = (page - 1) * recipesPerPage;
+  const end = page * recipesPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+  renderBtns(page, recipes.length, recipesPerPage);
 };
