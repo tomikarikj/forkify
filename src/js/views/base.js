15acdd779918d15138398445c400d5199807f67a
Tomi Karikj <karikjtomi@gmail.com>
2019-09-24 21:36:14 +0200

    Added a few new elements in the array

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -3,7 +3,8 @@ export const elements = {
   searchInput: document.querySelector('.search__field'),
   searchResults: document.querySelector('.results'),
   resultsList: document.querySelector('.results__list'),
-  resultsPages: document.querySelector('.results__pages')
+  resultsPages: document.querySelector('.results__pages'),
+  recipe: document.querySelector('.recipe')
 };
 
 export const classNames = {
