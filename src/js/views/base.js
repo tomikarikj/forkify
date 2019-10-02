9b9e86718abe66822fac6f98ca092585d0380564
Tomi Karikj <karikjtomi@gmail.com>
2019-10-02 19:47:17 +0200

    Added the selector for the shopping list

--- src/js/views/base.js
+++ src/js/views/base.js
@@ -4,7 +4,8 @@ export const elements = {
   searchResults: document.querySelector('.results'),
   resultsList: document.querySelector('.results__list'),
   resultsPages: document.querySelector('.results__pages'),
-  recipe: document.querySelector('.recipe')
+  recipe: document.querySelector('.recipe'),
+  shoppingList: document.querySelector('.shopping__list')
 };
 
 export const classNames = {
